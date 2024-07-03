cfe32443d7a9417ed2d3f639bb5c169fe513cbb5
samadovoybek758 <oybek22samadov@gmail.com>
2024-07-03 16:32:13 +0500

    second

--- js/index.js
+++ js/index.js
@@ -35,3 +35,87 @@ console.log(x);
 var x = 5;
 // bu yerda chaqirilganda esa javob chiqamoqda chunki u uzgaruvchidan keyin turibdi
 console.log(x);
+
+
+
+
+// 3-masala
+// TDZ (Temporal Dead Zone) yani biz uzgaruvchi tayilashdan oldin uni chaqirsak u hoistingga uchraydi va uzgaruvchilar
+// kutaradi.shu kutarilishdan sung birinchi chaqirilganda qiymatdan foydalana olmaydi shu foydalana olmasligi
+// yoki shu foydalana olmaydigan hudud Temporal Dead Zone deb ataladi(o'lik hudud)
+console.log(z);
+var z = 6;
+console.log(z);
+
+
+// 5-masala
+
+const student = {
+    nmae : "alica",
+    age : 25,
+    course: "Computer Science"
+}
+// for...in  bu loopni obyektlar uchu qulash qulayroq.U obyektgagi kalitni ham hususiyatni ham 
+// ekranga chiqarishga yordam beradi 
+// buni massive stringlar uchun ham qullasa buladi lekin obyekt uchun juda qulay
+for (const key in student) {
+    console.log(key + ":" + student[key]);
+};
+
+// for...of bu massiv,string va boshqalarni har bir elementini ushlashga va ekranga chiqarishga yordam beradi
+const grades = [90,85,88];
+
+for (const iterator of grades) {
+    console.log(iterator);
+}
+
+
+
+
+
+
+// 6-masala
+// birinchi bulim
+console.log('Task 1 :');
+
+// hoisting kutarilish
+function HoistExample() {
+    console.log(a);
+    var a = 5
+    console.log(a);
+}
+HoistExample()
+
+console.log("Task 2 :");
+
+function ScopeExample() {
+    var x = 10;
+    if (true) {
+        let y = 20;
+        const z =30;
+        // bu yerda x for scopida bulmasa ham ekranga chiqadi chunki x unish otasi bulgan fumction scopeda joylashgan
+        console.log(x);
+        console.log(y);
+        console.log(z);
+    }
+    console.log(x);
+    // console.log(y); bu yerda bularni ekrancha chiqaraman desangiz bu hatoik beradi chunki u funksiyaning
+    // ichidagi for scopeda joylashgan undan tashqariga chiqa olmaydi
+    // console.log(z)
+}
+ScopeExample()
+
+
+
+console.log("Task 3 :");
+
+// bu yerda objekt berilmoqda va for...in yordamida uning ichidagi marcha kalitlar va hususiyatlarni ekranga chiqarilmoqda
+const obj = {name:`Jone`,age:30}
+for (const prop in obj) {
+    console.log(prop + ":" + obj[prop]);
+}
+// bu yerda esa for...of bu esa massiv ichidagi har bir elementni ushalash va ekranga chiqarishga yordam beradi
+const arr =[10,20,30];
+for (const value of arr) {
+    console.log(value);
+}
